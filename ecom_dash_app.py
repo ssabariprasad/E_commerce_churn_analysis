bb6c5e192a8a8b55fdb1e1db74e5404ab3f89b8e
ssabariprasad <84036175+ssabariprasad@users.noreply.github.com>
2023-10-30 21:12:12 +0530

    Add files via upload

---
+++ ecom_dash_app.py
@@ -0,0 +1,100 @@
+# importing neccessary libraries
+import numpy as np
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import warnings
+warnings.filterwarnings('ignore')
+
+#setting page config
+st.set_page_config(page_title='E-COM Churn Analysis',page_icon=':chart_with_downwards_trend:',layout='wide')
+st.title(':white[E-Commerce Customer Churn Analysis Dashboard]')
+st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
+
+#Loading Data
+@st.cache_data
+def get_data():
+    return pd.read_csv('ecom_clean_data.csv')
+
+df=get_data()
+
+#Addding toggle to view structure of data
+data_toggle=st.toggle(label='View Data',value=False)
+if data_toggle:
+    st.dataframe(df.head())
+else:
+    pass
+
+#Creating sidebar filters
+st.sidebar.header("Filter the Customers by below facets" )
+gen=list(df['Gender'].unique())
+age_grp=list(df['Age_Group'].unique())
+col1, col2 = st.columns(2)
+gender=st.sidebar.multiselect('Filter Customer by Gender',gen,placeholder='Select Gender',default=gen)
+age_group=st.sidebar.multiselect('Filter Customer by Age Group',age_grp, placeholder='Select Age Group',default=age_grp)
+
+st.markdown('---')
+
+
+#Creating metrics for churn data
+df2=df.query("Gender == @gender and Age_Group == @age_group")
+# Check if the dataframe is empty:
+if df2.empty:
+    st.warning("No data available based on the current filter settings!")
+    st.stop() # This will halt the app from further execution.
+
+col1, col2,col3 = st.columns(3)
+with col1:
+     st.subheader(':green[Total Customers]:busts_in_silhouette:')
+     st.subheader(df2['Customer ID'].nunique())
+with col2:
+    st.subheader(':blue[Customers Left]:man-running:')
+    st.subheader( df2[df2['Churn']==1]['Customer ID'].nunique())
+percentage=((df2[df2['Churn']==1]['Customer ID'].nunique())/df['Customer ID'].nunique())*100
+percentage=round(percentage)
+with col3:
+    st.subheader(':red[Customer Churn Rate]:chart_with_downwards_trend:')
+    st.subheader(str(percentage)+"%")
+
+st.markdown('---')
+
+#creating plots
+c1,c2=st.columns(2)
+with c1:
+     product_count=df2[df2['Churn']==1]['Product Category'].value_counts()
+     product_count=pd.DataFrame({'Category':product_count.index,'Number of Purchases':product_count.values})
+     fig2 = px.bar(product_count, x='Category', y='Number of Purchases',title='Purchase Made By Churn Customers in Different Product Category',color='Category',orientation='v')
+     st.plotly_chart(fig2, theme="streamlit", use_container_width=True)
+with c2:
+     st.dataframe(product_count,use_container_width=True,hide_index=True)
+     st.markdown('<style>div.block-container{padding-middle:1rem;}</style>',unsafe_allow_html=True)
+
+
+c11,c12=st.columns(2)
+with c11:
+    payment_type=df2[df2['Churn']==1]['Payment Method'].value_counts()
+    payment_type=pd.DataFrame({'Payment Type':payment_type.index,'Number of Purchases':payment_type.values})
+    fig1=px.pie(data_frame=payment_type,values='Number of Purchases',names='Payment Type',title='Payment Method Preffered by Churn Customers',color='Payment Type')
+    st.plotly_chart(fig1,theme="streamlit", use_container_width=True)
+with c12:
+     st.dataframe(payment_type,use_container_width=True,hide_index=True) 
+
+c21,c22=st.columns(2)
+with c21:
+    returns=df2[df2['Churn']==1].groupby('Product Category')['Returns'].sum()
+    returns=pd.DataFrame(dict(Product_Category=returns.index,Number_of_Returns=returns.values))
+    fig4 = px.bar(returns,x='Number_of_Returns', y='Product_Category',title='Products Retrurned By Churn Customers',color='Product_Category',orientation='h')
+    st.plotly_chart(fig4, theme="streamlit", use_container_width=True)
+with c22:
+    returns=df2.groupby('Product Category')['Returns'].sum()
+    st.dataframe(returns,use_container_width=True)  
+
+# ---- HIDE STREAMLIT STYLE ----
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
---
+++ readme.md
@@ -0,0 +1,32 @@
+**Project Title:** E-commerce Customer Churn Analysis
+
+**Project Description:**
+
+In this data analysis project, we aim to understand and analyze customer churn in an e-commerce business. We will utilize the pandas library for data loading and cleaning, and employ data analysis and visualization techniques with pandas, plotly and streamlit to gain insights into the factors contributing to customer churn.
+
+**Data Loading:**
+
+The first step in our analysis is data loading, which is achieved using the pandas library. We will load the dataset and prepare it for analysis.
+
+**Data Cleaning:**
+
+1. **Handling Missing Values:** We will begin by checking for missing values in the dataset. Our investigation reveals that the Returns column has missing values. To address this, we employ a statistical imputation technique by replacing the missing values with the mode (most frequent) value of the Returns column. This ensures that we have a complete dataset to work with.
+
+2. **Duplicate Rows and Columns:** We check for both duplicate rows and columns in the dataset. It is discovered that the Age column contains duplicate data, which serves no analytical purpose. Therefore, we remove the duplicated Age column, streamlining the dataset.
+
+3. **Outlier Detection:** Outliers can significantly affect our analysis. To ensure robust results, we apply the Box plot technique to check for outliers in the Customer Age and Total Purchase Amount columns. Our examination concludes that there are no outliers in these two columns.
+
+**Churn Analysis Dashboard:**
+
+With clean and well-prepared data, we move on to the core of our project - data analysis. We focus our analysis on the data related to customers who have left, as understanding the reasons behind customer churn is crucial.
+
+We will utilize pandas for data loading and exploration, and plotly for interactive data visualization. 
+Streamlit is used here for Dashobard Web app development. Through these tools, we will create informative visualizations and conduct in-depth analysis to uncover patterns, trends, and potential factors contributing to customer churn.
+
+By the end of this project, we aim to provide actionable insights and recommendations to help the e-commerce business reduce customer churn and improve overall customer satisfaction. Our analysis will be based on sound data cleaning and visualization techniques, enabling data-driven decisions for the organization.
+
+File Information:
+ECOM_Churn_Case_Study.ipynb   -> In this file, data cleaning and exported cleaned using pandas.
+ecom_dash_app.py  -> Stored clean data is loaded and dashboard app developed using streamlit.
+ecom_data.csv -> Raw Data
+ecom_clean_data.csv -> Cleaned Data
